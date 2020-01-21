6f0fc5390ddb3c09d90517a769610d92e42bad6a
yusufalafid <yusufucup48@gmail.com>
2020-01-21 05:01:59 +0000

    add yaml

---
+++ etcd.sh
@@ -0,0 +1,16 @@
+ETCD_VER=v3.3.18
+
+# choose either URL
+GOOGLE_URL=https://storage.googleapis.com/etcd
+GITHUB_URL=https://github.com/etcd-io/etcd/releases/download
+DOWNLOAD_URL=${GOOGLE_URL}
+
+rm -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+rm -rf /tmp/etcd-download-test && mkdir -p ~/exam/
+
+curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C ~/exam --strip-components=1
+rm -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+
+~/exam/etcd --version
+~/exam/etcdctl version
